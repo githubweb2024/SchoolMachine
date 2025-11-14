10d56c29873cf99dd177ff760cedd02d16adee74
KIBER_CODER <168917703+githubweb2024@users.noreply.github.com>
2025-11-14 20:55:32 +0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,11 @@ from backend.routers import lessons
 
 import uvicorn
 
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "routers"))
+
+
 app = FastAPI()
 
 init_db()
